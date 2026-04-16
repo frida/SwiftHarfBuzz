a87e6c273e55fc98f4f70d7b5334b36bcd36e853
Ole André Vadla Ravnås <oleavr@frida.re>
2026-04-16 15:36:39 +0200

    Point package deps at frida forks

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["HarfBuzz"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/gir2swift.git",    branch: "development"),
-        .package(url: "https://github.com/rhx/SwiftGObject.git", branch: "development"),
+        .package(url: "https://github.com/frida/gir2swift.git",    branch: "development"),
+        .package(url: "https://github.com/frida/SwiftGObject.git", branch: "development"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
